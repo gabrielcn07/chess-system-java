31394e28413cfb4818368cdbf098ecc210eddd29
Gabriel Nunes <gabrielchagasn@gmail.com>
2022-07-21 20:13:10 -0300

    BoardException and defensive programming

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,44 +1,63 @@
 package boardgame;
 
 public class Board {
-	
+
 	private Integer rows;
 	private Integer columns;
 	private Piece[][] pieces;
 
 	public Board(Integer rows, Integer columns) {
+		if (rows < 1 || columns < 1) {
+			throw new BoardException("Tabuleiro de tamanho invalido!");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
 	}
-	
+
 	public Piece piece(int row, int column) {
+		if(!positionExists(row, column)) {
+			throw new BoardException("Esta posição não existe no tabuleiro!");
+		}
 		return pieces[row][column];
 	}
-	
+
 	public Piece piece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Esta posição não existe no tabuleiro!");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
-	
+
 	public void placePiece(Piece piece, Position position) {
+		if(thereIsAPiece(position)) {
+			throw new BoardException("Jogada invalida, esta posição ja esta ocupada por uma peça");
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row <= rows && column >= 0 && column <= columns;
+	}
+
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+
+	public boolean thereIsAPiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Esta posição não existe no tabuleiro!");
+		}
+		return piece(position) != null;
 	}
 
 	public Integer getRows() {
 		return rows;
 	}
 
-	public void setRows(Integer rows) {
-		this.rows = rows;
-	}
-
 	public Integer getColumns() {
 		return columns;
 	}
 
-	public void setColumns(Integer columns) {
-		this.columns = columns;
-	}
-	
 }
